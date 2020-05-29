c5893bb488985c3a218a98153a45f050e44315c8
Melih Keskin <melihkeskinn@gmail.com>
2020-05-29 14:08:25 +0200

    vue-looping-with-v_for

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 new Vue({
   el: "#app", //element
   data: {
-    title: "Becoming a Ninja turtle", // property
-    showName: false,
-    showAge: true
+    title: "Becoming a Ninja turtle", // property 
+    items:['mushroom', 'green shells', 'red shells', 'banana','start'],
+    ninjas:[
+        {name:'Melih1', age:31, belt:'Black'},
+        {name:'Melih2', age:32, belt:'Black'},
+        {name:'Melih3', age:33, belt:'Black'},
+        {name:'Melih4', age:34, belt:'Black'},
+        {name:'Melih5', age:35, belt:'Black'}
+    ]
   },
-  methods: {
-    toggleName() {
-      this.showName = !this.showName;
-    },
-    toggleAge() {
-        this.showAge = !this.showAge;
-      },
+  methods: { 
   },
 });
