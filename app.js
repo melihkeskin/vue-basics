9bd494d43bdacd3e80843a009155fd325b0973ef
Melih Keskin <melihkeskinn@gmail.com>
2020-05-29 12:18:32 +0200

    vue-data-binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: "#app", //element
   data: {
     title: "Becoming a Vue ninja", // property
-    name: 'Melih'
+    name: 'Melih',
+    url:'http://www.youtube.com',
+    classes:['one', 'two']
   },
   methods: {
       greet(time){
