90eedc430656731a1c751915547b507a100168b9
Melih Keskin <melihkeskinn@gmail.com>
2020-05-29 12:08:33 +0200

    vue-methods

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ new Vue({
   el: "#app", //element
   data: {
     title: "Becoming a Vue ninja", // property
+    name: 'Melih'
   },
+  methods: {
+      greet(time){
+          //this.title
+          //ES6
+          return `Hello and ${time}, ${this.name}`
+      }
+  }
 });
